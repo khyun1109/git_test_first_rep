8eab11284619eafc402030bcb0d84a0c84564f52
khyun1109 <khyun1109@gmail.com>
2019-05-13 22:40:27 +0900

    Add cal.py
    print calculating

---
+++ cal.py
@@ -0,0 +1,15 @@
+from plus import plus
+from minus import minus
+from divide import divide
+from multiply import multiply
+
+a = input("Enter your number: ")
+a = int(a)
+b = input("Enter your number: ")
+b = int(b)
+
+print("===============")
+plus(a,b)
+minus(a,b)
+divide(a,b)
+multiply(a,b)
